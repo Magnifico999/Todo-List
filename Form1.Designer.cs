8f7937df678e40134a98626455228ed2d94c6101
Elmagn1f1co <steveamadi007@gmail.com>
2024-10-11 09:26:35 +0100

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace EF_Todo_list
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dgvTask = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnDis = new System.Windows.Forms.Button();
+            this.TaskID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Activities = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Priority = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ExpiryDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTask)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(16, 235);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 37);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(100, 235);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 37);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(181, 233);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 39);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(12, 297);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 39);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Activities";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Priority";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Status";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Expiry Date";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(113, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 29);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(113, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(196, 29);
+            this.textBox3.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Completed",
+            "Pending",
+            "Uncompleted"});
+            this.comboBox1.Location = new System.Drawing.Point(113, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(196, 29);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(113, 166);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 29);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // dgvTask
+            // 
+            this.dgvTask.AllowUserToDeleteRows = false;
+            this.dgvTask.BackgroundColor = System.Drawing.Color.Snow;
+            this.dgvTask.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTask.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TaskID,
+            this.Activities,
+            this.Priority,
+            this.Status,
+            this.ExpiryDate});
+            this.dgvTask.Location = new System.Drawing.Point(343, 26);
+            this.dgvTask.Name = "dgvTask";
+            this.dgvTask.ReadOnly = true;
+            this.dgvTask.Size = new System.Drawing.Size(545, 412);
+            this.dgvTask.TabIndex = 3;
+            this.dgvTask.DoubleClick += new System.EventHandler(this.dgvTask_DoubleClick);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(113, 303);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(200, 29);
+            this.txtSearch.TabIndex = 15;
+            // 
+            // btnDis
+            // 
+            this.btnDis.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDis.Location = new System.Drawing.Point(16, 358);
+            this.btnDis.Name = "btnDis";
+            this.btnDis.Size = new System.Drawing.Size(75, 37);
+            this.btnDis.TabIndex = 16;
+            this.btnDis.Text = "Display";
+            this.btnDis.UseVisualStyleBackColor = true;
+            this.btnDis.Click += new System.EventHandler(this.btnDis_Click);
+            // 
+            // TaskID
+            // 
+            this.TaskID.DataPropertyName = "TaskID";
+            this.TaskID.HeaderText = "TaskID";
+            this.TaskID.MinimumWidth = 4;
+            this.TaskID.Name = "TaskID";
+            this.TaskID.ReadOnly = true;
+            this.TaskID.Visible = false;
+            // 
+            // Activities
+            // 
+            this.Activities.DataPropertyName = "Activities";
+            this.Activities.HeaderText = "Activities";
+            this.Activities.Name = "Activities";
+            this.Activities.ReadOnly = true;
+            // 
+            // Priority
+            // 
+            this.Priority.DataPropertyName = "Priority";
+            this.Priority.HeaderText = "Priority";
+            this.Priority.Name = "Priority";
+            this.Priority.ReadOnly = true;
+            // 
+            // Status
+            // 
+            this.Status.DataPropertyName = "Status";
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            // 
+            // ExpiryDate
+            // 
+            this.ExpiryDate.DataPropertyName = "ExpiryDate";
+            this.ExpiryDate.HeaderText = "ExpiryDate";
+            this.ExpiryDate.Name = "ExpiryDate";
+            this.ExpiryDate.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(911, 450);
+            this.Controls.Add(this.btnDis);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dgvTask);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "EF Todo list";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTask)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dgvTask;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnDis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TaskID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Activities;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Priority;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ExpiryDate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EF_Todo_list
+{
+    public partial class Form1 : Form
+    {
+        Task model = new Task();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+        void clear()
+        {
+            textBox2.Text = textBox3.Text = comboBox1.Text = dateTimePicker1.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.TaskID = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clear();
+            populateDataGridView();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox2.Text) || string.IsNullOrWhiteSpace(comboBox1.Text) || string.IsNullOrWhiteSpace(dateTimePicker1.Text))
+                {
+                    MessageBox.Show("Please fill in all required fields");
+                }
+                else
+                {
+                    model.Activities = textBox2.Text.Trim();
+                    model.Priority = textBox3.Text.Trim();
+                    model.Status = comboBox1.Text.Trim();
+                    model.ExpiryDate = DateTime.Parse(dateTimePicker1.Text);
+                    using (EFDBEntities db = new EFDBEntities())
+                    {
+                        if (model.TaskID == 0)// Insert
+                            db.Tasks.Add(model);
+                        else // Update
+                            db.Entry(model).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    clear();
+                    populateDataGridView();
+                    MessageBox.Show("Submitted successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+        void populateDataGridView()
+        {
+            dgvTask.AutoGenerateColumns = false;
+            using (EFDBEntities db = new EFDBEntities())
+            {
+                dgvTask.DataSource = db.Tasks.ToList<Task>();
+            }
+        }
+
+        private void dgvTask_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvTask.CurrentRow.Index != -1)
+            {
+                model.TaskID = Convert.ToInt32(dgvTask.CurrentRow.Cells["TaskID"].Value);
+                using (EFDBEntities db = new EFDBEntities())
+                {
+                    model = db.Tasks.Where(x => x.TaskID == model.TaskID).FirstOrDefault();
+                    model.Activities = textBox2.Text.Trim();
+                    model.Priority = textBox3.Text;
+                    model.Status = comboBox1.Text;
+                    model.ExpiryDate = DateTime.Parse(dateTimePicker1.Text);
+                }
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this Record ?", "EF Todo list", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (EFDBEntities db = new EFDBEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.Tasks.Attach(model);
+                    db.Tasks.Remove(model);
+                    db.SaveChanges();
+                    populateDataGridView();
+                    clear();
+                    MessageBox.Show("Deleted successfully");
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (EFDBEntities db = new EFDBEntities())
+            {
+                string searchQuery = txtSearch.Text.Trim();
+                DateTime searchDate;
+                bool isDate = DateTime.TryParse(searchQuery, out searchDate);
+
+                var query = from t in db.Tasks
+                            where t.Activities.Contains(searchQuery)
+                            || t.Priority.Contains(searchQuery)
+                            || t.Status.Contains(searchQuery)
+                            || (isDate && t.ExpiryDate == searchDate)
+                            select t;
+
+                List<Task> results = query.ToList();
+                if (results.Count > 0)
+                {
+                    // Display search results
+                    dgvTask.DataSource = results;
+                    MessageBox.Show("Search completed successfully.");
+                }
+                else
+                {
+                    // No matching records found
+                    MessageBox.Show("No matching records found.");
+                }
+            }
+        }
+
+        private void btnDis_Click(object sender, EventArgs e)
+        {
+            populateDataGridView();
+        }
+    }
+}
